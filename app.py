74bdb27153d2387d35e2d584bcd9374dd89d3dde
Jessie Lin <b19950828@gmail.com>
2022-03-05 03:42:56 +0800

    3/5

--- app.py
+++ app.py
@@ -1,29 +1,162 @@
+from re import X
+from dotenv import load_dotenv
+import os
+import mysql.connector
+import json
 from flask import *
+# !/usr/bin/python
+# -*- coding: utf-8 - *-
+load_dotenv("mydb.evn")
 app = Flask(__name__)
 app.config["JSON_AS_ASCII"] = False
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
+mydb = mysql.connector.connect(
+    host="127.0.0.1", user=os.getenv("user"), password=os.getenv("password"), database="OriginRepository")
+mycursor = mydb.cursor()
+
 # Pages
 
 
-@app.route("/")
+@ app.route("/")
 def index():
     return render_template("index.html")
 
 
-@app.route("/attraction/<id>")
+@ app.route("/attraction/<id>")
 def attraction(id):
+    # val = (id,)
+    sqlSelect = "SELECT * FROM `ATTRACTIONS` WHERE `NUM`= %s;"
+    mycursor.execute(sqlSelect, (id,))
+    myresult = mycursor.fetchone()
+    # ---------
+    try:
+        if myresult == None:
+            error = {}
+            error["error"] = True
+            error["message"] = "自訂的錯誤訊息"
+            return (error, 400)
+        else:
+            values = {}
+            for num in range(len(myresult)):
+                columnName = mycursor.description[num][0]
+                columnValue = myresult[num]
+                values[columnName] = columnValue
+            valuesImagesSplitHTTP = values['images'].split(",")
+            values["images"] = valuesImagesSplitHTTP
+        # --------
+            print(values)
+            response = {}
+            response["data"] = values
+            return response
+    except:
+        error = {}
+        error["error"] = True
+        error["message"] = "自訂的錯誤訊息"
+        return (error, 500)
     return render_template("attraction.html")
 
 
-@app.route("/booking")
+@ app.route("/attractions")
+def attractions():
+    # ---------------- 計算筆數的函式
+    def howMany(string):
+        mycursor.execute(string)
+        count = mycursor.fetchone()
+        count = str(count)
+        count = int(count.replace(",", "").replace("(", "").replace(")", ""))
+        return count
+    # ---------------- 取出資料的函式
+
+    def howManyData(myresult):
+        # values = {}
+        valuesList = []
+        for infoNum in range(len(myresult)):  # len is 12
+            info = myresult[infoNum]
+            values = {}
+            for num in range(len(info)):
+                # values = {}
+                columnName = mycursor.description[num][0]
+                columnValue = info[num]
+                values[columnName] = columnValue
+            valuesList.append(values)
+        return valuesList
+    # ----------------
+
+    page = request.args.get("page", 0)
+    page = int(page)
+    pagiN = 12
+    keyword = request.args.get("keyword", None)
+
+    # -----------
+    try:
+        if keyword == None:
+            sqlCount = "SELECT CIUNT(NUM) FROM `ATTRACTIONS`"
+            count = howMany(sqlCount)
+            print("成功", count)
+            # --------
+            sqlSelect = "SELECT * FROM `ATTRACTIONS` LIMIT %s,%s;"
+            mycursor.execute(sqlSelect, (page*pagiN, pagiN))
+            myresult = mycursor.fetchall()
+            myresult = list(myresult)
+            finialData = howManyData(myresult)
+            # --------
+            response = {}
+            if count-(page*pagiN) > pagiN:
+                response["nextPage"] = page+1
+                response["data"] = finialData
+                return response
+            else:
+                response["nextPage"] = "null"
+                response["data"] = finialData
+                return response
+        else:
+            sqlCount = "SELECT COUNT(NUM) FROM `ATTRACTIONS` WHERE `STITLE` LIKE '%" + \
+                keyword+"%';"
+            count = howMany(sqlCount)
+            print("關鍵字比數成功", count)
+            # -------------
+            sqlSelect = "SELECT * FROM `ATTRACTIONS` WHERE `STITLE` like '%"+keyword+"%';"
+            mycursor.execute(sqlSelect,)
+            myresult = mycursor.fetchall()
+            myresult = list(myresult)
+            finialData = howManyData(myresult)
+            print("執行完韓式", finialData)
+            # --------------
+            response = {}
+            if page*pagiN < count and count-(page*pagiN) > pagiN:
+                response["nextPage"] = page+1
+                response["data"] = finialData
+                return response
+            elif page*pagiN < count and count-(page*pagiN) < pagiN:
+                response["nextPage"] = "null"
+                response["data"] = finialData
+                return response
+            else:
+                page = str((count//pagiN)+1)
+                print(page)
+                error = {}
+                error["error"] = True
+                error["message"] = "最多只到第"+page+"頁"
+                return (error, 400)
+            # -------------
+
+    except:
+        error = {}
+        error["error"] = True
+        error["message"] = "自訂的錯誤訊息"
+        return (error, 500)
+
+
+@ app.route("/booking")
 def booking():
     return render_template("booking.html")
 
 
-@app.route("/thankyou")
+@ app.route("/thankyou")
 def thankyou():
     return render_template("thankyou.html")
 
 
-app.run(port=3000)
+if __name__ == "__main__":
+    app.run(port=3000, debug=True)
---
+++ data/attractionsData.py
@@ -0,0 +1,60 @@
+import json
+import mysql.connector
+import os
+from dotenv import load_dotenv
+load_dotenv("mydb.evn")
+
+# -----------
+
+mydb = mysql.connector.connect(
+    host="127.0.0.1", user=os.getenv("user"), password=os.getenv("password"), database="OriginRepository")
+# if mydb.is_connected():
+#     print("ok!")
+mycursor = mydb.cursor()
+# mycursor.execute("SELECT DATABASE();")
+# record = mycursor.fetchone()
+# print("目前使用的資料庫：", record)
+
+# -----------
+mycursor.execute("SHOW TABLES LIKE 'ATTRACTIONS';")
+result = mycursor.fetchall()
+
+if result == []:
+    dataRead = open('data/taipei-attractions.json', mode='r').read()
+    dataLoads = json.loads(dataRead)
+    data = dataLoads['result']['results']
+
+    dataLens = (len(data))
+    for num in range(dataLens):
+        Url = []
+        dataUrl = data[num]["file"].lower()
+        dataSplitHTTP = dataUrl.split("https")
+        for x in range(len(dataSplitHTTP)):
+            if dataSplitHTTP[x].endswith('jpg') == True:
+                Url.append("https"+dataSplitHTTP[x])
+        replaceUrl = str(Url).replace("'", " ")
+        newUrl = replaceUrl.strip('[]')
+        imagesValues = {'images': newUrl}
+        data[num].update(imagesValues)
+        del data[num]["file"]
+
+    datakeys = list(data[0].keys())
+    datakeys = str(datakeys)
+    datakeys = datakeys.replace("'", "").replace("[", "(").replace("]", ")")
+
+    for num in range(dataLens):
+        dataValues = list(data[num].values())
+        # insertMysql = '''INSERT INTO `ATTRACTIONS`
+        #         (info ,  stitle ,  xpostDate ,  longitude ,  REF_WP ,
+        #         avBegin ,  langinfo ,  MRT ,  SERIAL_NO ,  RowNumber ,
+        #         CAT1 ,  CAT2 ,  MEMO_TIME ,  POI ,  idpt ,  latitude ,
+        #         xbody ,  _id ,  avEnd ,  address , images )
+        #         VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'''
+        insertMysql = "INSERT INTO `ATTRACTIONS`"+datakeys + \
+            "VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);"
+        mycursor.execute(insertMysql, dataValues)
+        mydb.commit()
+        print(mycursor.rowcount, "record inserted.")
+        mydb.close()
+else:
+    mydb.close()
