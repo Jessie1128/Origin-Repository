37c884c646f5a4e7bfe365bfebb6f20be271d86b
Jessie Lin <b19950828@gmail.com>
2022-03-17 09:08:14 +0800

    3/17 week-11

--- static/index.js
+++ static/index.js
@@ -1,3 +1,7 @@
+// var idList={};
+// let home_page=document.getElementsByClassName("nav_left")[0];
+// home_page.addEventListener("click",homePage);
+
 indexPage = (page) => {
     let url=`/api/attractions?page=${page}`;
     fetchUrl(url);
@@ -13,15 +17,68 @@ select = () => {
     }
 };
 
+attractionPage = () =>{
+    let id=location.pathname;
+    id=id.substring(id.lastIndexOf("/")+1);
+    fetch(`/api/attraction/${id}`)
+    .then(res=>res.json())
+    .then((data)=>{
+        let pic=data.data["images"][0];
+        let name=data.data["name"];
+        let mrt=data.data["mrt"];
+        let cat=data.data["category"];
+        let des=data.data["description"]
+        let address=data.data["address"];
+        let trans=data.data["transport"];
+        console.log(name);
+        document.getElementsByClassName("section_left")[0].setAttribute("src",pic);
+        document.getElementsByClassName("attraction_name")[0].textContent=name;
+        document.getElementsByClassName("attraction_loca")[0].textContent=`${cat} at ${mrt}`;
+        document.getElementsByTagName('p')[5].textContent=des;
+        document.getElementsByTagName('p')[7].textContent=address;
+        document.getElementsByTagName('p')[9].textContent=trans;
+    }) 
+    eventListener();
+}
+
+homePage = () => {
+    console.log("goooood");
+    window.location.href="/";
+}
+
+eventListener = () =>{
+    let home_page=document.getElementsByClassName("nav_left")[0];
+    home_page.addEventListener("click",homePage);
+    // ===========================================================
+    let circle_first=document.getElementsByClassName("blue_circle")[0];
+    let circle_second=document.getElementsByClassName("blue_circle")[1];
+    console.log(circle_first,circle_second);
+    circle_first.addEventListener("click",circle_click,false);
+    circle_second.addEventListener("click",circle_click,false); 
+} 
+
+circle_click = (click) => {
+    let right_bottom_sapn2=document.getElementsByClassName("right_bottom_sapn2")[0];
+    if(click.target==document.getElementsByClassName("blue_circle")[0]){
+        right_bottom_sapn2.textContent="新台幣 2000 元";
+    }else{
+        right_bottom_sapn2.textContent="新台幣 2500 元";
+    }
+}
+
+// -----------------------------------------------------------
+
+
+// -----------------------------------------------------------
+
 fetchUrl = (url) => {
     fetch(url,{method: 'GET'})
-    .then((response)=>{
-        return response.json();
+    .then((res)=>{
+        return res.json();
     })
     .then((data)=>{
         if(data["error"]){
-            url='';
-            appendElements(data,url);
+            appendElements(data,url='');
             return data;          
         }else{
             nextPage=data.nextPage;
@@ -63,9 +120,49 @@ fetchUrl = (url) => {
         observer.observe(newTarget);
         let botton = document.getElementById("search");
         botton.addEventListener("click",select);
+        // -----------------------------------------------------------
         }
     })
 };
+// ------回應前端頁面------/attraction/${id}--------------------
+mouseover = (click) => {
+    let keyword = click.target.parentNode.childNodes[1].innerText;
+    get_id_by_keyword(keyword);
+}
+
+
+async function get_id_by_keyword(keyword){
+    let data = await fetch(`api/attractions?keyword=${keyword}`);
+    data = await data.json();
+    id=data.data[0]["id"];
+    window.location.href = `/attraction/${id}`;
+}
+
+
+get_data_by_id = (data) =>{
+    let id=data.data[0]["id"];
+    let pic=data.data[0]["images"];
+    let name=data.data[0]["name"];
+    let mrt=data.data[0]["mrt"];
+    let cat=data.data[0]["category"];
+    let des=data.data[0]["description"]
+    let address=data.data[0]["address"];
+    let trans=data.data[0]["transport"];
+    return (id,pic,name,mrt);
+}   
+
+append_item = () => {
+
+}
+
+
+
+
+
+// -----------------------------------------------------------
+
+
+
 
 appendElements = (data,url) => {
     let attraction=document.createElement("div");
@@ -94,7 +191,6 @@ appendElements = (data,url) => {
     }else{
         if(0<document.getElementsByClassName("attraction")["length"]){
             attraction.style.marginTop="-25px";
-           footer.setAttribute("style", position="absoulte");
         }
         append(data,attraction);
         document.body.insertBefore(attraction,footer);
@@ -102,6 +198,7 @@ appendElements = (data,url) => {
 };
 
 append = (data,attraction) => {
+    console.log(data);
     num=0;
     for(num;num<data.data.length;num++){
         let attraction_box=document.createElement("div");
@@ -130,6 +227,13 @@ append = (data,attraction) => {
         cat.appendChild(catMrt);
         cat.appendChild(catCat);
         attraction.appendChild(attraction_box);
+        // -------------------------------------------
+        // idList[text.textContent]=data.data[num]["id"];
+        attraction_box.addEventListener("click",mouseover,false);
+        // console.log("現在在這邊",attraction_box);
     }
     return attraction;
 };
+
+
+// -----------------------------------------------------------
