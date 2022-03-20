706fd3edd8a915333b4aff1234050f245c1369a3
Jessie Lin <b19950828@gmail.com>
2022-03-20 18:54:43 +0800

    3/20 week-11

--- static/index.js
+++ static/index.js
@@ -1,7 +1,3 @@
-// var idList={};
-// let home_page=document.getElementsByClassName("nav_left")[0];
-// home_page.addEventListener("click",homePage);
-
 indexPage = (page) => {
     let url=`/api/attractions?page=${page}`;
     fetchUrl(url);
@@ -23,22 +19,28 @@ attractionPage = () =>{
     fetch(`/api/attraction/${id}`)
     .then(res=>res.json())
     .then((data)=>{
-        let pic=data.data["images"][0];
+        let pic=data.data["images"];
         let name=data.data["name"];
         let mrt=data.data["mrt"];
         let cat=data.data["category"];
         let des=data.data["description"]
         let address=data.data["address"];
         let trans=data.data["transport"];
-        console.log(name);
-        document.getElementsByClassName("section_left")[0].setAttribute("src",pic);
+        // document.getElementsByClassName("section_left")[0].setAttribute("src",pic);
         document.getElementsByClassName("attraction_name")[0].textContent=name;
         document.getElementsByClassName("attraction_loca")[0].textContent=`${cat} at ${mrt}`;
         document.getElementsByTagName('p')[5].textContent=des;
         document.getElementsByTagName('p')[7].textContent=address;
         document.getElementsByTagName('p')[9].textContent=trans;
-    }) 
+        // return pic;
+        slider(pic);
+    })
+    // .then((pic)=>{
+    //     eventListener();
+    //     slider(pic);
+    // })
     eventListener();
+//     slider(pic);
 }
 
 homePage = () => {
@@ -66,8 +68,127 @@ circle_click = (click) => {
     }
 }
 
-// -----------------------------------------------------------
+slider = (pic) => {
+    // =============================================================create img
+    let slides=document.createElement("div");
+    for (num=0;num<pic.length;num++) {
+        let slides_img=document.createElement("img");
+        slides.className="slides";
+        slides_img.setAttribute("src",pic[num]);
+        slides_img.className="slides_img";
+        slides.appendChild(slides_img);
+    }
+    section_left.appendChild(slides);
 
+    // =============================================================create manual radio
+    let manual=document.createElement("div");
+    manual.className="manual";
+    for(num=0;num<pic.length;num++) {
+        let manual_label=document.createElement("label");
+        manual_label.htmlFor=`radio${num+1}`;
+        manual_label.className="manual_label";
+        manual.appendChild(manual_label);
+        console.log(manual_label);
+        console.log(manual);
+        manual_label.addEventListener("click",slide_change_img)
+    }
+    section_left.appendChild(manual);
+    old_botton=document.getElementsByClassName("manual_label")[0];
+    old_botton.style.background="#ffffff";
+    // =============================================================create change pic icon
+    let change_icon=document.createElement("div");
+    change_icon.className="change_icon";
+    for(num=0;num<2;num++){
+        let icon_cursor=document.createElement("div");
+        icon_cursor.className="icon_cursor";
+        let arrows=document.createElement("div");
+        arrows.className="arrow_left";
+        icon_cursor.appendChild(arrows);
+        change_icon.appendChild(icon_cursor);
+        icon_cursor.addEventListener("click",arrow_change_icon);
+    }
+    section_left.appendChild(change_icon);
+    document.getElementsByClassName("arrow_left")[1].className="arrow_right";
+}
+// =============================================================silder effect
+var pic_num=[];
+slide_change_img = (click) => {
+    let img_width=document.getElementById("section_left").offsetWidth;
+    console.log("圖片這格大小現在",img_width);
+    // =============================================================================img_width
+    let botton=document.querySelectorAll(".manual_label");
+    console.log(botton.length);
+    botton.forEach((ele)=>ele.style.backgroundColor="transparent");
+    // =============================================================================arrow botton effect
+    if(click=="right"){
+            // console.log('pic_num["num"]',pic_num["num"]);
+            // console.log('manual_label',manual_label)
+        if(pic_num["num"]===undefined){
+            pic_num["num"]=1;
+        }
+        n_p=pic_num["num"];
+        manual_label=document.getElementsByClassName("manual_label")[n_p];
+        if(manual_label===undefined){
+            n_p-=n_p;
+            console.log(n_p);
+            manual_label=document.getElementsByClassName("manual_label")[n_p];
+        }
+        manual_label.style.backgroundColor="#ffffff";
+        newSlide=document.getElementsByClassName("slides_img")[0];
+        newSlide.style.marginLeft=`-${n_p*img_width}px`;
+        pic_num["num"]=n_p+1;
+        return;
+    }else if(click=="left"){
+        console.log("left");
+        if(pic_num["num"]===undefined){
+            pic_num["num"]=(botton.length)+1;
+        }
+        n_p=pic_num["num"];
+        console.log(n_p-2);
+        manual_label=document.getElementsByClassName("manual_label")[n_p-2];
+        if(manual_label===undefined){
+            n_p=botton.length+1;
+        }
+        manual_label=document.getElementsByClassName("manual_label")[n_p-2];
+        manual_label.style.backgroundColor="#ffffff";
+        newSlide=document.getElementsByClassName("slides_img")[0];
+        newSlide.style.marginLeft=`-${(n_p-2)*img_width}px`;
+        pic_num["num"]=n_p-1;
+        console.log(pic_num["num"]);
+        return;
+    }
+    // ========================================================================end of arrow botton effect
+    let start_Page=1;
+    let num=click.target["attributes"][0]['nodeValue'];
+    click.target.style.backgroundColor="#ffffff";
+    num=Number(num.toString().replace("radio",""));
+    pic_num["num"]=num;
+    console.log(pic_num);
+    // ==========================================
+    if(num==1){
+        newSlide=document.getElementsByClassName("slides_img")[0];
+        newSlide.style.marginLeft="0px";
+    }else if(start_Page<num){
+        move_page=num-start_Page;
+        newSlide=document.getElementsByClassName("slides_img")[0];
+        newSlide.style.marginLeft=`-${move_page*img_width}px`;
+    }else{
+        return;
+    }
+}
+
+arrow_change_icon = (click) => {
+    click=click.target["className"];
+    if(click=="arrow_right"){
+        right="right";
+        slide_change_img(right);
+    }else if(click=="arrow_left"){
+        left="left";
+        slide_change_img(left);
+    }else{
+        return;
+    }
+}
 
 // -----------------------------------------------------------
 
@@ -123,13 +244,13 @@ fetchUrl = (url) => {
         // -----------------------------------------------------------
         }
     })
-};
+}
+
 // ------回應前端頁面------/attraction/${id}--------------------
 mouseover = (click) => {
     let keyword = click.target.parentNode.childNodes[1].innerText;
     get_id_by_keyword(keyword);
 }
-
 
 async function get_id_by_keyword(keyword){
     let data = await fetch(`api/attractions?keyword=${keyword}`);
@@ -137,7 +258,6 @@ async function get_id_by_keyword(keyword){
     id=data.data[0]["id"];
     window.location.href = `/attraction/${id}`;
 }
-
 
 get_data_by_id = (data) =>{
     let id=data.data[0]["id"];
@@ -151,18 +271,7 @@ get_data_by_id = (data) =>{
     return (id,pic,name,mrt);
 }   
 
-append_item = () => {
-
-}
-
-
-
-
-
 // -----------------------------------------------------------
-
-
-
 
 appendElements = (data,url) => {
     let attraction=document.createElement("div");
@@ -228,12 +337,9 @@ append = (data,attraction) => {
         cat.appendChild(catCat);
         attraction.appendChild(attraction_box);
         // -------------------------------------------
-        // idList[text.textContent]=data.data[num]["id"];
         attraction_box.addEventListener("click",mouseover,false);
-        // console.log("現在在這邊",attraction_box);
     }
     return attraction;
 };
-
 
 // -----------------------------------------------------------
