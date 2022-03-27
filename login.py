b395bf2fd180e18132aeae4a0270127aa3e154ad
Jessie Lin <b19950828@gmail.com>
2022-03-27 23:30:33 +0800

    3/27

--- login.py
+++ login.py
@@ -97,8 +97,7 @@ def loginPage():
             print("'user_token'", user_token)
             print("'user_token'type", type(user_token))
             # ==============
-            res = make_response(
-                jsonify({"ok": True, "user_token": user_token}), 200)
+            res = make_response({"ok": True, "user_token": user_token}, 200)
             res.set_cookie('user_token', value=user_token, samesite="Lax")
             print("res", res)
             print("restype", type(res))
